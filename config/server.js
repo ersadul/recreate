938a5724390f7a222b9cb3cffca81f0485b2decb
Ersa Sa'dul <ersasadulashar@gmail.com>
2023-05-30 21:16:58 +0700

    update: integrate strapi with GCS

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://recreate-bakffz27rq-et.a.run.app'),
+  url: env('URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
