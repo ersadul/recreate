54eed62c4cf32221350bb9a78ffce21796d5765a
Ersa Sa'dul <ersasadulashar@gmail.com>
2023-06-10 16:40:24 +0700

    Update: user, history, and recommendation History: build history content-type and add relation to user User: add relation to History Recommendation: automate create new history and update unique history when user fetch data

--- src/api/recommendation/controllers/recommendation.js
+++ src/api/recommendation/controllers/recommendation.js
@@ -6,4 +6,40 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::recommendation.recommendation');
+module.exports = createCoreController('api::recommendation.recommendation', ({ strapi }) => ({
+    async find(ctx) {
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+
+        // custom logic store recommendations to history
+        const userId = ctx.state.user.id;
+
+        // fetch histories data
+        const histories = await strapi.entityService.findMany('api::history.history', {
+            fields: ['*'],
+            populate: ['user', 'recommendations'],
+        });
+
+        // check if authenticated user have any history or not
+        const isHistoryExist = await strapi.service('api::recommendation.recommendation')
+            .isHistoryExist(userId, histories);
+
+        if (!isHistoryExist) {
+            // create new history from data recommendation fetched data
+            await strapi.entityService.create('api::history.history', {
+                data: {
+                    user: ctx.state.user,
+                    recommendations: data,
+                    publishedAt: new Date().getTime(),
+                },
+            });
+        }
+        else {
+            // update history if there is any new data fetched by user
+            await strapi.service('api::recommendation.recommendation')
+                .updateHistory(userId, data);
+        }
+
+        return { data, meta };
+    },
+}));
