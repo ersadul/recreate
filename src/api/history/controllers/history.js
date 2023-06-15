bff4d6e0e6126b246037463877f780d58ee255d6
Ersa Sa'dul <ersasadulashar@gmail.com>
2023-06-16 03:10:50 +0700

    Update: history logic

--- src/api/history/controllers/history.js
+++ src/api/history/controllers/history.js
@@ -6,4 +6,48 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::history.history');
+module.exports = createCoreController('api::history.history', ({ strapi }) => ({
+    async create(ctx) {
+
+        const userId = ctx.state.user.id;
+        const { articles, videos } = ctx.request.body.data;
+
+        const histories = await strapi.entityService.findMany('api::history.history', {
+            fields: ['*'],
+            populate: ['user', 'articles', 'videos'],
+        });
+
+        const isHistoryExist = await strapi.service('api::article.article')
+            .isHistoryExist(userId, histories);
+
+        if (!isHistoryExist) {
+            // create new history from data article fetched data
+            const data = {
+                user: userId,
+                articles: articles,
+                videos: videos,
+                publishedAt: new Date().getTime(),
+            }
+            await strapi.entityService.create('api::history.history', {
+                data
+            });
+
+            return {
+                message: "New history created!"
+            }
+        }
+        else {
+            // update history if there is any new data fetched by user
+            const statusArticles = await strapi.service('api::history.history')
+                .updateHistoryArticle(userId, articles);
+            const statusVideos = await strapi.service('api::history.history')
+                .updateHistoryVideos(userId, videos);
+
+            const message = statusArticles || statusVideos ? "Item(s) updated" : "Nothing updated";
+
+            return {
+                message: message
+            }
+        }
+    },
+}));
--- src/api/history/services/history.js
+++ src/api/history/services/history.js
@@ -6,4 +6,104 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::history.history');
+module.exports = createCoreService('api::history.history', ({ strpi }) => ({
+    async isHistoryExist(...args) {
+        for (let history of args[1]) {
+            if (args[0] == history.user.id) {
+                return true
+            }
+        }
+        return false
+    },
+    async updateHistoryArticle(...args) {
+        const userId = args[0]
+
+        const userEntry = await strapi.query('plugin::users-permissions.user').findOne({
+            select: ['id'],
+            where: { id: userId },
+            populate: { history: true },
+        });
+
+        const historyId = userEntry.history.id
+
+        const oldHistoryEntry = await strapi.query('api::history.history').findOne({
+            select: ['id'],
+            where: { id: historyId },
+            populate: { articles: true },
+        });
+
+        // old history
+        const arr1 = oldHistoryEntry.articles;
+        // new history coming form request
+        const arr2 = args[1];
+
+        const haveSameItems = arr1.length === arr2.length && arr2.every(element => arr1.map(obj => obj.id).includes(element));
+
+
+        if (!haveSameItems) {
+            if (arr2.every(element => arr1.map(obj => obj.id).includes(element))) {
+                return false;
+            }
+            // merged old and new history
+            const mergedHistory = arr1.concat(arr2);
+            // extract id
+            const idValues = mergedHistory.map(obj => (typeof obj === "object") ? obj.id : obj);
+            // extract unique ids
+            const uniqueIds = [...new Set(idValues)].map(id => ({ id }));
+            await strapi.entityService.update('api::history.history', historyId, {
+                data: {
+                    articles: uniqueIds
+                },
+            });
+
+            return true;
+        }
+
+        return false;
+    },
+    async updateHistoryVideos(...args) {
+        const userId = args[0]
+
+        const userEntry = await strapi.query('plugin::users-permissions.user').findOne({
+            select: ['id'],
+            where: { id: userId },
+            populate: { history: true },
+        });
+
+        const historyId = userEntry.history.id
+
+        const oldHistoryEntry = await strapi.query('api::history.history').findOne({
+            select: ['id'],
+            where: { id: historyId },
+            populate: { videos: true },
+        });
+
+        // old history
+        const arr1 = oldHistoryEntry.videos
+        // new history coming form request
+        const arr2 = args[1]
+
+        const haveSameItems = arr1.length === arr2.length && arr2.every(element => arr1.map(obj => obj.id).includes(element));
+
+        if (!haveSameItems) {
+            if (arr2.every(element => arr1.map(obj => obj.id).includes(element))) {
+                return false;
+            }
+            // merged old and new history
+            const mergedHistory = arr1.concat(arr2);
+            // extract id
+            const idValues = mergedHistory.map(obj => (typeof obj === "object") ? obj.id : obj);
+            // extract unique ids
+            const uniqueIds = [...new Set(idValues)].map(id => ({ id }));
+            await strapi.entityService.update('api::history.history', historyId, {
+                data: {
+                    videos: uniqueIds
+                },
+            });
+
+            return true;
+        }
+
+        return false;
+    },
+}));
